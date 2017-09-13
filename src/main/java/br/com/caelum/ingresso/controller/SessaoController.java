8016e20c289941a6651f45a727a85b7c326e2657
Rafael Gatti <rafaelgatti@claretiano.edu.br>
2017-09-13 09:18:23 -0300

    Action para acessar lugares

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -65,4 +66,13 @@ public class SessaoController {
 		
 		return form(form.getSalaId(), form);
 	}
+	
+	@GetMapping("/sessao/{id}/lugares")
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId){
+		
+		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		
+		return modelAndView;
+	}
+	
 }
